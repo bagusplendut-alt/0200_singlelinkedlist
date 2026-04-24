47acad4957fdb7df23c69177c411eea9ca780f7a
bagusplendut-alt <bagusplendut@gmail.com>
2026-04-24 16:05:12 +0700

    menambahkan bool

--- linklist.cpp
+++ linklist.cpp
@@ -29,26 +29,59 @@ public:
         if (START == NULL || nim <= START->noMHS)
         {
             if ((START != NULL) && (nim == START->noMHS))
-        {
+            {
             cout << "\nDuplikasi noMHS tidak di ijinkan\n";
             return;
-        }
-        nodeBaru->next = START;
-        START = nodeBaru;
-        return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
 
-        Node *previous = START;
-        Node *current = START;
-        while ((current != NULL) && (nim >= current->noMHS))
-        {
-        if (nim == current->noMHS)
-        {
-            cout << "\nDuplikasi noMHS tidak di ijinkan\n";
-            return;
+            Node *previous = START;
+            Node *current = START;
+            while ((current != NULL) && (nim >= current->noMHS))
+            {
+            if (nim == current->noMHS)
+            {
+                cout << "\nDuplikasi noMHS tidak di ijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+            }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
         }
-        previous = current;
-        current = current->next;
-    }
-    nodeBaru->next = current;
-    previous->next = nodeBaru;
-}
+
+        bool listEmpty()
+        {
+            return (START == NULL);
+        }
+
+        bool Search(int nim, Nodeprevious, Node current)
+        {
+            *previous = START;
+            *current = START;
+
+            while ((current != NULL) && (nim != (*current)->noMhs))
+            {
+                *previous = current;
+                *current = (current)->next;
+            }
+            return (*current != NULL);
+        }
+
+        bool delNode(int nim)
+        {
+            Node *current, *previous;
+            if (!Search(nim, &previous, &current))
+                return false;
+
+            if (current == START)
+                START = START->next;
+            else
+                previous->next = current->;
+            delete current;
+            return true;
+        }
